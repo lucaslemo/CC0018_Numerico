c5eba1e400771d45a27faa855b7686c9db75760f
Pedro Henrique <lopes.pedro@aluno.ufca.edu.br>
2021-07-04 15:55:50 -0300

    Update trajetoria.py

--- trajetoria.py
+++ trajetoria.py
@@ -2,8 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 delta = np.float64(2**-53)
 x = np.linspace(0, 24)
-y = np.linspace(0, 24)
+y = np.linspace(6*delta+delta, 34*delta+delta)
+y1 = np.linspace(6*delta+delta, 34*delta+delta)
 y = y - delta
-plt.axis([0, 30*delta, 0, 30*delta])
-plt.plot(x, x, label = "reta da parede")
-plt.plot(y, x, label = "reta do robô")
+z = [0, 6*delta]
+z1 = [17*delta, 7*delta]
+plt.axis([0, 50*delta, 0, 50*delta])
+plt.plot(x, x)
+plt.plot(y, y1, color='green')
+plt.plot(z,z1, color='green')
+plt.legend(['reta da parede', 'reta do robô'], loc=9)
+plt.show()
